c2d13d43787722211e67500f5a477345d31d6176
BritishPandaChick <sbartley1080@gmail.com>
2021-12-23 13:30:55 -0500

    Add global variables. Create functions to fetch JSON data and user info

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Global variables
+//Selectors overview div where profile information will appear
+const overview = document.querySelector(".overview");
+const username = "BritishPandaChick";
+
+//fetch API JSON data 
+const gitUserInfo = async function () {
+    const userInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+//Fetch and Display User Information
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `<figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> `;
+  overview.append(div);
+};
