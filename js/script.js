1ae96470fc9a29e17351da873d2123cee3995a2d
BritishPandaChick <sbartley1080@gmail.com>
2021-12-26 23:00:17 -0500

    Add functions to fetch repo and display info

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 //Selectors overview div where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "BritishPandaChick";
+//Select repo list
+const reposList = document.querySelector(".repo-list");
 
 //fetch API JSON data 
 const gitUserInfo = async function () {
@@ -16,15 +18,34 @@ gitUserInfo();
 const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
-    div.innerHTML = `<figure>
-    <img alt="user avatar" src=${data.avatar_url} />
-  </figure>
+    div.innerHTML = 
+    `<figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
 
-  <div>
-    <p><strong>Name:</strong> ${data.name}</p>
-    <p><strong>Bio:</strong> ${data.bio}</p>
-    <p><strong>Location:</strong> ${data.location}</p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-  </div> `;
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> `;
   overview.append(div);
+  gitRepos();
+};
+
+//Fetch your repos 
+const gitRepos = async function () {
+  const fetchRepos = await fetch(`http://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+};
+
+//Display info about your repos 
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    reposList.append(repoItem);
+  }
 };
